493e10a9f879437df99fcbb5d49e18584f5c10a7
Matteo Rampolla <37956397+MRampo@users.noreply.github.com>
2022-11-25 11:31:37 +0100

    Small Fix in the comments

--- CommandLine.sh
+++ CommandLine.sh
@@ -1,10 +1,7 @@
 maxLoc=$(cut -f5 -d, bank_transactions.csv | sort | uniq -c | sort |tail -n 1)
-
-M=$(awk -F, '$4=="M"{x+=$9;}END{print x}' bank_transactions.csv)
-
-F=$(awk -F, '$4=="F"{x+=$9;}END{print x}' bank_transactions.csv)
-
-average=$(awk -F, '{date1[$2]+=$9;date2[$2]+=1}END{for (key in date1) print key, date1[key]/date2[key]}' bank_transactions.csv | sort -k2 -n -r| head -n 1)
+M=$(awk -F "," '$4=="M"{x+=$9;}END{print x}' bank_transactions.csv)
+F=$(awk -F "," '$4=="F"{x+=$9;}END{print x}' bank_transactions.csv)
+average=$(awk -F, '{date1[$2]+=$9;date2[$2]+=1}END{for (key in date1) print key, date1[key]/date2[key]}' bank_transactions.csv | sort -k2 -n -r| head -n 5)
 
 
 echo $maxLoc
